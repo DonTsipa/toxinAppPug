3648a538b9e4cdb758ab7365625004051bb0a9c4
don.tsipa <don_tsipa@mail.ru>
2019-12-26 00:36:32 +0300

    saving sent filters - done.

--- routes/search.js
+++ routes/search.js
@@ -12,22 +12,25 @@ const findRoom = databaseBlock.findRoom;
 const router = express.Router();
 
 router.post('/search',urlencodedParser,(req,res)=>{
-  const DateFrom = req.body.indexDatefrom;
-  const DateTo = req.body.indexDateto;
+  let DateFrom = req.body.indexDatefrom;
+  let DateTo = req.body.indexDateto;
   if(req.body.dateRange){
     DateFrom = req.body.dateRange.split('-')[0]
     DateTo = req.body.dateRange.split('-')[1]
   }
-  const smoking = req.body.smoking;
-  const pets = req.body.pets;
-  const friends = req.body.friends;
-  const wide_coridor = req.body.wide_coridor;
-  const disabled_assistant = req.body.disabled_assistant;
-  const guests = {'numberGuests':req.body.numberGuests,'adults':req.body.adults,'children':req.body.children,'babies':req.body.babies}
+  let smoking = req.body.smoking;
+  let pets = req.body.pets;
+  let friends = req.body.friends;
+  let wide_coridor = req.body.wide_coridor;
+  let disabled_assistant = req.body.disabled_assistant;
+  let guests = {'numberGuests':req.body.numberGuests,'adults':req.body.adults,'children':req.body.children,'babies':req.body.babies}
+  if(smoking) smoking = true;
+  if(pets) pets = true;
+  if(friends) friend = true;
+  if(wide_coridor) wide_coridor = true;
+  if(disabled_assistant) disabled_assistant = true;
   //const rooms = findRoom(req.body);
   const facilities =  {'numberFacilities':req.body.numberFacilities,'bedrooms':req.body.bedrooms,'beds':req.body.beds,'bathrooms':req.body.bathrooms}
-  
- 
   // indexes = findRoom([guests, DateFrom,DateTo]);
  const id = req.session.userId;
  const name = req.session.Name;
@@ -35,12 +38,13 @@ router.post('/search',urlencodedParser,(req,res)=>{
  res.render('search.pug',
   {
     'DateFrom':DateFrom, 'DateTo':DateTo, 'guests':guests,
+    'smoking':smoking,'pets':pets,'friends':friends,'wide_coridor':wide_coridor,'disabled_assistant':disabled_assistant,
     user:{
       id,
       login,
       name
-    }
-
+    },
+    facilities
   });
 })
 router.get('/search',(req,res)=>{
