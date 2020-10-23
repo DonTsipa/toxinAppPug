1f9a1e5ae9b42a61b94ca1f08fad4c384a35cf46
DonTsipa <54895675+DonTsipa@users.noreply.github.com>
2020-10-23 20:33:52 +0300

    pagination is working

--- app/blocks/develop/header/header.js
+++ app/blocks/develop/header/header.js
@@ -1,9 +1,13 @@
-const button = document.querySelector('.header__menuButton');
-button.addEventListener('click', () => {
-  const menu = document.querySelector('.header__navigation');
+let menu_expand = false;
+const menuButton = document.querySelector('.header__menuButton');
+const menu = document.querySelector('.header__navigation');
+menuButton.addEventListener('click', () => {
   if (menu.classList.contains('header__navigation_active')) {
     menu.classList.remove('header__navigation_active')
+    menu_expand = false;
   } else {
     menu.classList.add('header__navigation_active')
+    menu_expand = true;
   }
 })
+
